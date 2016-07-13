2aea8a2001c14a42f35180272b406cd476b88514
NegHao <15099902013@126.com>
2016-07-13 15:32:25 +0800

    iOS基础框架
    快速开发

---
+++ NHBaseFramework/Main/NHConfigHeader.h
@@ -0,0 +1,15 @@
+//
+//  NHConfigHeader.h
+//  NHBaseProjectFramework
+//
+//  Created by simope on 16/7/13.
+//  Copyright © 2016年 NegHao.W. All rights reserved.
+//
+
+#ifndef NHConfigHeader_h
+#define NHConfigHeader_h
+
+#define  KEY_USERNAME_PASSWORD @"com.NegHao.NHBaseProjectFramework"
+
+
+#endif /* NHConfigHeader_h */
---
+++ NHBaseFramework/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NHBaseFramework
+//
+//  Created by simope on 16/7/13.
+//  Copyright © 2016年 NegHao.W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
