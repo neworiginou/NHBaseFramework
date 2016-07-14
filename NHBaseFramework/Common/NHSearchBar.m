3e00c9fe7e9a8610776c592ac8b33e6a364772c6
NegHao <15099902013@126.com>
2016-07-14 15:37:21 +0800

    添加几个库和更新宏定义文件
    1，添加MJRefresh刷新库；
    2，宏定义文件增加几个常用定义，并更改不规范的宏名称；

---
+++ NHBaseFramework/Common/NHSearchBar.m
@@ -0,0 +1,28 @@
+//
+//  NHSearchBar.m
+//  NHCarTribe
+//
+//  Created by simope on 16/7/13.
+//  Copyright © 2016年 NegHao.W. All rights reserved.
+//
+
+#import "NHSearchBar.h"
+
+@implementation NHSearchBar
+
+- (instancetype)initWithFrame:(CGRect)frame{
+
+    if (self = [super initWithFrame:frame]) {
+        [self addSearchBar];
+    }
+    return self;
+}
+
+
+- (void)addSearchBar{
+
+    UITextField *search = [[UITextField alloc] init];
+}
+
+
+@end
---
+++ NHBaseFramework/First Interface/controller/NHFirstViewController.m
@@ -0,0 +1,30 @@
+//
+//  NHFirstViewController.m
+//  NHCarTribe
+//
+//  Created by simope on 16/7/13.
+//  Copyright © 2016年 NegHao.W. All rights reserved.
+//
+
+#import "NHFirstViewController.h"
+
+@interface NHFirstViewController ()
+
+@end
+
+@implementation NHFirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+
+
+}
+
+
+
+
+
+
+@end
